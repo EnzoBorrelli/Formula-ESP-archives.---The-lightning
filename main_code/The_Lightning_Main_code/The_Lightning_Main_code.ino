b2d388a1153295f91d02503bd178133fead63be3
Enzo Borrelli <johndoe@example.comenzoborrellidev@gmail.com>
2022-09-07 15:38:37 -0300

    actualizaciones menores

--- main_code/The_Lightning_Main_code/The_Lightning_Main_code.ino
+++ main_code/The_Lightning_Main_code/The_Lightning_Main_code.ino
@@ -1,11 +1,53 @@
-/*  -----------
-   The LIghtning
-   ------------
+/* ____________________________________
+   Formula ESP archives - The Lightning
+   ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 
 Codigo principal del proyecto 
 
                                Enzo G. Borrelli A. - 2022 
  */                                                         
+
+
+
+class Motores{
+private:
+
+public:
+Motores(){
+
+}
+
+};
+
+class Stepper{
+private:
+
+public:
+Stepper(){
+
+}
+
+};
+
+class Led_rgb{
+private:
+
+public:
+Led_rgb(){
+
+}
+
+};
+
+class Buzzer{
+private:
+
+public:
+Buzzer(){
+
+}
+
+};
 
 void setup() {
   // put your setup code here, to run once:
