b975e1c14eccff24ee488e86c57ded3dc0e229e0
Enzo Borrelli <johndoe@example.comenzoborrellidev@gmail.com>
2022-09-07 15:20:51 -0300

    se agrega el archivo principal de codigo (WIP)

---
+++ main_code/The_Lightning_Main_code/The_Lightning_Main_code.ino
@@ -0,0 +1,18 @@
+/*  -----------
+   The LIghtning
+   ------------
+
+Codigo principal del proyecto 
+
+                               Enzo G. Borrelli A. - 2022 
+ */                                                         
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
